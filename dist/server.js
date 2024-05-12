98df955f677290376782ce063ff595a618771d3b
Joe Wolfe <JoeWolfe0527@gmail.com>
2024-05-12 19:18:49 -0400

    Maybe trying to use a build system is a bad idea. I'm going to try this.

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app.js"));
-var _config = require("./config.js");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_app.default.listen(_config.PORT, function () {
-  console.log(`Server starting on http://localhost:${_config.PORT}`);
-});
